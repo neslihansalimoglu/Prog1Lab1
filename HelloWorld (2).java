99e598ce067d9fb393263f754f997d979f3893f7
Neslihan Salimoglu <neslisalim2@gmail.com>
2020-02-03 09:38:42 -0500

    prints out HelloWorld

--- HelloWorld (2).java
+++ HelloWorld (2).java
@@ -1,10 +1,8 @@
-public class Age {
+public class HelloWorld (2) {
 
     public static void main(String[] args) {
-        // Prints " Age" to the terminal window.
-         int birthday = 2000;
-         int currentyear = 2020;
-         int age = currentyear - birthday;
-    System.out.println("my age is" + age);
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
     }
+
 }
