82bfa27c1f04dc0b90ed2804c120353eb329c8c6
Neslihan Salimoglu <neslisalim2@gmail.com>
2020-02-03 11:18:03 -0500

    Loop for square root

---
+++ LoopExercise.java
@@ -0,0 +1,11 @@
+public class LoopExercise{
+	public static void main(String[] args) {
+		int number;
+		int end_value = 9;
+
+		for (number = 1; number <=9; number++){
+			System.out.println("the square of" + number + "is" + (int)Math.pow(number,2));
+		}
+
+	}
+}
