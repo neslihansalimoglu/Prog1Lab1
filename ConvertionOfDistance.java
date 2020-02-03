811f19525aaec205f19fddcf1e06d009990e5074
Neslihan Salimoglu <neslisalim2@gmail.com>
2020-02-03 10:22:24 -0500

    meter to feet convertion

---
+++ ConvertionOfDistance.java
@@ -0,0 +1,10 @@
+public class ConvertionOfDistance{
+	public static void main(String[] args) {
+		double  meter;
+		double  feet;
+
+		meter = 4.6;
+		feet = 3.28084 * meter; 
+		System.out.println("The distance is " + feet);
+	}
+}
