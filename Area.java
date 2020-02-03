62d902a43247014243a9c0e641a84573f865fd73
Neslihan Salimoglu <neslisalim2@gmail.com>
2020-02-03 09:52:54 -0500

    calculated the Area of a rectangle

---
+++ Area.java
@@ -0,0 +1,15 @@
+public class Area {
+
+    public static void main(String[] args) {
+        // Prints "Area of a rectangle"
+    	int Length;
+    	int Height;
+    	int Area;
+
+    	Length = 18; 
+    	Height = 7;
+    	Area = Length * Height;
+        System.out.println("The Area is " + Area);
+    }
+
+}
