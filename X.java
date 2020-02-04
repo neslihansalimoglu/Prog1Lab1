5d3a6e6e07229cdac8bdc2055a9f8f2c56732d09
Neslihan Salimoglu <neslisalim2@gmail.com>
2020-02-03 19:51:33 -0500

    prints out X composed of asterisks

---
+++ X.java
@@ -0,0 +1,14 @@
+public class X {
+    
+       public static void main(String[] args) {
+          System.out.println();
+          System.out.println("\n        *   *");
+          System.out.println("\n         * *");
+          System.out.println("\n          *");
+          System.out.println("\n         * *");
+          System.out.println("\n        *   *");
+         
+          System.out.println();
+       }  // end main()
+
+    }  // end class
